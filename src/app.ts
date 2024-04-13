2001d9576cf66dc5b557f82a2977db5f128149b3
sonsu95 <tjdtn951007@gmail.com>
2024-04-13 23:47:13 +0900

    feat: add channels

--- src/app.ts
+++ src/app.ts
@@ -5,19 +5,38 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 const port = process.env.PORT;
-const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_TOKEN || '';
-const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID || '';
+const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN || '';
+const TEST_CHANNEL_1 = process.env.TEST_CHANNEL_1 || '';
+const TEST_CHANNEL_2 = process.env.TEST_CHANNEL_2 || '';
+const TEST_CHANNEL_3 = process.env.TEST_CHANNEL_3 || '';
+const TEST_CHANNEL_4 = process.env.TEST_CHANNEL_4 || '';
 
 app.post('/tradingview-webhook', async (req, res) => {
-  const { message } = req.body;
+  const { message, data } = req.body;
   try {
-    await axios.post(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`, {
-      chat_id: Number(TELEGRAM_CHAT_ID),
+    await axios.post(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`, {
+      chat_id: TEST_CHANNEL_1,
       text: message,
     });
+    await axios.post(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`, {
+      chat_id: TEST_CHANNEL_2,
+      text: message,
+    });
+
+    await axios.post(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`, {
+      chat_id: TEST_CHANNEL_3,
+      text: message,
+    });
+
+    await axios.post(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`, {
+      chat_id: TEST_CHANNEL_4,
+      text: message,
+    });
+    console.log('data: ', data);
     res.status(200).send('Alert sent to Telegram');
 
   } catch (error) {
+    console.log('test, test');
     res.status(500).send('Error sending message to Telegram');
   }
 });
