88d66965e5f32ab1f136eadc77d5bcfad7e5392c
sonsu95 <tjdtn951007@gmail.com>
2024-04-14 01:53:54 +0900

    feat: 동시성 문제 해결

--- src/services/channelServices.ts
+++ src/services/channelServices.ts
@@ -1,21 +1,25 @@
 import axios from 'axios';
 import { TELEGRAM_TOKEN, TEST_CHANNELS } from '../configs.ts';
 
-const sendMessage = async (message: string) => {
+export const sendMessage = async (message: string) => {
   const promises = TEST_CHANNELS.map(channel => {
     return axios.post(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`, {
       chat_id: channel,
       text: message,
+    }).then(() => {
+      console.log(`Message sent to channel ${channel}`);
+    }).catch(error => {
+      console.error(`Failed to send message to channel ${channel}:`, error);
     });
   });
 
   try {
     await Promise.all(promises);
-    console.log('All messages sent successfully');
+    console.log('All messages attempted to send');
   } catch (error) {
-    console.error('Failed to send messages:', error);
-    throw new Error('Error sending messages to Telegram');
+    console.error('Some messages may not have been sent:', error);
   }
 };
 
-export { sendMessage };
+
+
