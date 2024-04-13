5034e9d43dbc448bc66e3e852d8eaee145096c64
sonsu95 <tjdtn951007@gmail.com>
2024-04-14 03:28:50 +0900

    feat: 빌드에러 수정

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import { sendMessage } from './services/channelServices';
 import { PORT, TEST_CHANNELS_1, TEST_CHANNELS_2, TEST_CHANNELS_3, TEST_CHANNELS_4 } from './configs';
 
-
 const app = express();
 app.use(express.json());
 app.use(express.text({ type: 'text/plain' }));
@@ -14,6 +13,7 @@ app.post('/tradingview-webhook', async (req, res) => {
     await sendMessage(TEST_CHANNELS_2, message);
     await sendMessage(TEST_CHANNELS_3, message);
     await sendMessage(TEST_CHANNELS_4, message);
+
     res.status(200).send('Alert sent to Telegram');
   } catch (error) {
     console.error('Error sending message to Telegram:', error);
