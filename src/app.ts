19917ce72b8d926fb2f98c7f975fd640f6a3c176
sonsu95 <tjdtn951007@gmail.com>
2024-04-13 22:46:00 +0900

    feat: update tradingview-webhook alert dynamic message

--- src/app.ts
+++ src/app.ts
@@ -9,15 +9,15 @@ const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_TOKEN || '';
 const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID || '';
 
 app.post('/tradingview-webhook', async (req, res) => {
-  const { price, name } = req.body;
+  const { message } = req.body;
   try {
     await axios.post(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`, {
       chat_id: Number(TELEGRAM_CHAT_ID),
-      text: `${name}의 가격이 ${price}의 아래입니다.`,
+      text: message,
     });
     res.status(200).send('Alert sent to Telegram');
+
   } catch (error) {
-    console.log('TOKEN', TELEGRAM_BOT_TOKEN);
     res.status(500).send('Error sending message to Telegram');
   }
 });
